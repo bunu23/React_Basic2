55a27330b306521838f2f2851b031a326e30a3c1
Bunu <bunubhattarai39@gmail.com>
2024-01-29 20:09:30 -0500

    app added

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,18 @@
-import React,{useState, useEffect} from 'react';
-import './App.css';
-import Header from "./Header";
-import AddContact from "./AddContact";
-import ContactList from "./ContactList";
+import React from "react";
+import "./App.css"
 
-function App() {
-  const LOCAL_STORAGE_KEY="contacts";
-const[contacts, setContacts]=useState([]);
-const addContactHanler=(contact)=>{
-  console.log(contact);
-  setContacts([...contacts, contact]);
+function App(){
+
+return(
+  <div>
+    Hello World
+  
+  </div>
+
+)
+
+
 }
 
-useEffect(
-()=>
-{
-  localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(contacts));
-}, [contacts]
-
-);
-
-useEffect(
-  ()=>
-  {
-   const retrieveContacts=JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-   if(retrieveContacts) setContacts(retrieveContacts);
-  }, []
-  
-  );
-
-  return (
-  
-
-<div className='ui container'>
-
-<Header/>
-<AddContact addContactHanler={addContactHanler}/>
-<ContactList contacts={contacts}/>
-
-</div>
-
-  );
-}
 
 export default App;
